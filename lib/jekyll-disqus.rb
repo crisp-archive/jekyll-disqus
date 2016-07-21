8c2285e83871e679f099cf4808d36f08c6a5bd8b
David Zhang <crispgm@gmail.com>
2016-07-21 15:24:49 +0800

    Read from _config.yml

--- lib/jekyll-disqus.rb
+++ lib/jekyll-disqus.rb
@@ -9,16 +9,20 @@ module Jekyll
 
     def render(context)
       if @disqus_name.empty?
-        @disqus_name = context.registers[:disqus]
+        @disqus_name = context.registers[:site].config["disqus"]["username"]
       end
+
+      return "" if @disqus_name.empty?
       
       html = <<-EOB
+<script>
 var disqus_shortname = '#{@disqus_name}';
 (function() {
   var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
   dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
   (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
 })();
+</script>
 EOB
       html
     end
